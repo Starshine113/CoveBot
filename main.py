7b57b3ac042088640854cd55fca3dcf413fade23
Emily <emily@zukrosi.org>
2020-09-22 19:25:29 +0200

    make logging level configurable

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if os.environ.get("COVEBOT_DATABASE"):
 
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
+logger.setLevel(bot_config["bot"]["logging_level"])
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
